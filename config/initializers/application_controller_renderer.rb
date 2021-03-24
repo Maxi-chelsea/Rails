383d554fad45ab24f680321f73d5314917431723
Maxi-chelsea <maxi19950049@gmail.com>
2021-03-24 14:46:01 +0000

    [Finish]7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd328ec5ad919ff1bd9fa9bd834bade5bcfe09ba0f577c3113e83d5e91a3d8e36ff8176eb242ee858d2f2f54b51627fb4a1cd6fcf8fd2e5a0175fa52d13fa46ce'
